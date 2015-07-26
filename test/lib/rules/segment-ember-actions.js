74e84e4556314efb68960892cbadbf05e7df0e7b
Chris Manson <chris@manson.ie>
2015-07-26 17:55:27 +0100

    allowing for analytics inside if statements

--- test/lib/rules/segment-ember-actions.js
+++ test/lib/rules/segment-ember-actions.js
@@ -11,45 +11,61 @@ var ESLintTester = require("eslint-tester");
 var eslintTester = new ESLintTester(linter);
 
 eslintTester.addRuleTest("lib/rules/segment-ember-actions", {
-    valid: [{
-        code: "import Ember from 'ember';\nexport default Ember.Controller.extend({\n  actions: {\n    exampleAction() {\n      analytics.track('somthing', {with: 'data'}); \n    }\n  }\n});",
-        ecmaFeatures: {
-            "modules": true
+    valid: [
+        {
+            code: "import Ember from 'ember';\nexport default Ember.Controller.extend({\n  actions: {\n    exampleAction() {\n      analytics.track('somthing', {with: 'data'}); \n    }\n  }\n});",
+            ecmaFeatures: {
+                "modules": true
+            },
+            "env": {
+                "browser": true,
+                "es6": true
+            }
         },
-        "env": {
-            "browser": true,
-            "es6": true
+        {
+            code: "import Ember from 'ember';\nexport default Ember.Controller.extend({\n  actions: {\n    exampleAction() {\n      if(true) { analytics.track('somthing', {with: 'data'}); } \n    }\n  }\n});",
+            ecmaFeatures: {
+                "modules": true
+            },
+            "env": {
+                "browser": true,
+                "es6": true
+            }
+        },
+        {
+            code: "import Ember from 'ember';\nexport default Ember.Controller.extend({\n  actions: {\n    //no-track\n exampleAction() {}\n  }\n});",
+            ecmaFeatures: {
+                "modules": true
+            },
+            "env": {
+                "browser": true,
+                "es6": true
+            }
+        },
+        {
+            code: "import Ember from 'ember';\nexport default Ember.Controller.extend({\n  notActions: {\n    exampleAction() {\n      notTheRightFunction.track('somthing', {with: 'data'}); \n    }\n  }\n});",
+            ecmaFeatures: {
+                "modules": true
+            },
+            "env": {
+                "browser": true,
+                "es6": true
+            }
         }
-    }, {
-        code: "import Ember from 'ember';\nexport default Ember.Controller.extend({\n  actions: {\n    //no-track\n exampleAction() {}\n  }\n});",
-        ecmaFeatures: {
-            "modules": true
-        },
-        "env": {
-            "browser": true,
-            "es6": true
+    ],
+    invalid: [
+        {
+            code: "import Ember from 'ember';\nexport default Ember.Controller.extend({\n  actions: {\n    exampleAction() {}\n  }\n});",
+            ecmaFeatures: {
+                "modules": true
+            },
+            "env": {
+                "browser": true,
+                "es6": true
+            },
+            errors: [{
+                message: "no analytics.track() found for action \"exampleAction\""
+            }]
         }
-    }, {
-        code: "import Ember from 'ember';\nexport default Ember.Controller.extend({\n  notActions: {\n    exampleAction() {\n      notTheRightFunction.track('somthing', {with: 'data'}); \n    }\n  }\n});",
-        ecmaFeatures: {
-            "modules": true
-        },
-        "env": {
-            "browser": true,
-            "es6": true
-        }
-    }],
-    invalid: [{
-        code: "import Ember from 'ember';\nexport default Ember.Controller.extend({\n  actions: {\n    exampleAction() {}\n  }\n});",
-        ecmaFeatures: {
-            "modules": true
-        },
-        "env": {
-            "browser": true,
-            "es6": true
-        },
-        errors: [{
-            message: "no analytics.track() found for action \"exampleAction\""
-        }]
-    }]
+    ]
 });
